94ad0110c6c9c80eab2cf3e15b2af8269bffb3f4
smasiowski <masiowskisl@g.cofc.edu>
2017-03-16 18:15:10 -0400

    done! :)

--- app/src/main/java/com/introtoandroid/stockquotes_masiowski/MainActivity.java
+++ app/src/main/java/com/introtoandroid/stockquotes_masiowski/MainActivity.java
@@ -1,13 +1,181 @@
 package com.introtoandroid.stockquotes_masiowski;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.io.IOException;
+import android.widget.TextView;
+import android.content.Context;
+import android.widget.Toast;
+
+
+//================================================================================================//
 
 public class MainActivity extends AppCompatActivity {
 
+    // VARIABLE DECLARATIONS======================================================================//
+
+    String input;
+    String symbol;
+    String name;
+    String tradePrice;
+    String tradeTime;
+    String change;
+    String range;
+
+    EditText editText;
+    Button button;
+
+    TextView symbolTV;
+    TextView nameTV;
+    TextView tradePriceTV;
+    TextView tradeTimeTV;
+    TextView changeTV;
+    TextView rangeTV;
+
+
+    boolean invalidSymbol = false;
+    Stock stock;
+    Context context;
+
+
+    // ONCREATE===================================================================================//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        symbolTV = (TextView) findViewById(R.id.symbol);
+        nameTV = (TextView) findViewById(R.id.name);
+        tradePriceTV = (TextView) findViewById(R.id.tradePrice);
+        tradeTimeTV = (TextView) findViewById(R.id.tradeTime);
+        changeTV = (TextView) findViewById(R.id.change);
+        rangeTV = (TextView) findViewById(R.id.range);
+
+        context = getApplicationContext();
+
+
+        // ONCLICK--------------------------------------------------------------------------------//
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                stockQuote();
+            }
+        });
     }
+
+    // CALCULATE STOCKQUOTE=======================================================================//
+    public void stockQuote(){
+
+        input = editText.getText().toString();
+
+        if (input.length()>0){
+
+            stock = new Stock(input);
+
+            // NEW THREAD-------------------------------------------------------------------------//
+            new Thread() {
+                public void run() {
+
+                    // ERROR HANDLING
+                    try {
+                        stock.load();
+                    }
+                    catch (IOException ex) {
+                        Toast.makeText(context, "IO EXCEPTION",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    catch (Exception ex){
+                        Toast.makeText(context, "GENERAL EXCEPTION",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    // GET DATA FROM STOCK CLASS
+                    symbol = stock.getSymbol();
+                    name = stock.getName();
+                    tradePrice = stock.getLastTradePrice();
+                    tradeTime = stock.getLastTradeTime();
+                    change = stock.getChange();
+                    range = stock.getRange();
+
+
+                    if (stock.getName().contains("/")) {
+
+                        invalidSymbol = true;
+
+                        symbol = null;
+                        name = null;
+                        change = null;
+                        range = null;
+
+                    }
+                }
+
+            }.start();
+        }
+        else{
+            invalidSymbol = true;
+        }
+
+
+        symbolTV.setText(symbol);
+        nameTV.setText(name);
+        tradePriceTV.setText(tradePrice);
+        tradeTimeTV.setText(tradeTime);
+        changeTV.setText(change);
+        rangeTV.setText(range);
+
+
+        // ERROR MESSAGE
+        if(invalidSymbol == true) {
+            Toast.makeText(context, "Not a valid stock", Toast.LENGTH_LONG).show();
+            invalidSymbol=false;
+
+        }
+
+
+
+    }
+
+
+
+    // SAVE INSTANCE STATE========================================================================//
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("input",editText.getText().toString());
+        outState.putString("symbol",symbolTV.getText().toString());
+        outState.putString("name",nameTV.getText().toString());
+        outState.putString("tradePrice",tradePriceTV.getText().toString());
+        outState.putString("tradeTime",tradeTimeTV.getText().toString());
+        outState.putString("change",changeTV.getText().toString());
+        outState.putString("range",rangeTV.getText().toString());
+
+    }
+
+    // RESTORE INSTANCE STATE=====================================================================//
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        editText.setText(savedInstanceState.getString("input"));
+        symbolTV.setText(savedInstanceState.getString("symbol"));
+        nameTV.setText(savedInstanceState.getString("name"));
+        tradePriceTV.setText(savedInstanceState.getString("tradePrice"));
+        tradeTimeTV.setText(savedInstanceState.getString("tradeTime"));
+        changeTV.setText(savedInstanceState.getString("change"));
+        rangeTV.setText(savedInstanceState.getString("range"));
+    }
+
+
 }
--- app/src/main/java/com/introtoandroid/stockquotes_masiowski/Stock.java
+++ app/src/main/java/com/introtoandroid/stockquotes_masiowski/Stock.java
@@ -4,10 +4,10 @@ package com.introtoandroid.stockquotes_masiowski;
  * Created by user on 3/15/17.
  */
 
-
-        import android.util.Log;
-        import java.net.*;
-        import java.io.*;
+import android.annotation.SuppressLint;
+import android.util.Log;
+import java.net.*;
+import java.io.*;
 
 
 public class Stock implements Serializable
@@ -20,13 +20,14 @@ public class Stock implements Serializable
 
     private static final String TAG_PREFIX = "edu.cofc.stock";
 
-    private String symbol;
-    private String lastTradeTime;
-    private String lastTradePrice;
-    private String change;
-    private String range;
-    private String name;
+    public String symbol;
+    public String lastTradeTime;
+    public String lastTradePrice;
+    public String change;
+    public String range;
+    public String name;
 
+    public boolean badName = false;
 
     public Stock(String symbol)
     {
@@ -37,8 +38,8 @@ public class Stock implements Serializable
     }
 
 
-    public void load() throws MalformedURLException, IOException
-    {
+    //@SuppressLint("LongLogTag")
+    public void load() throws MalformedURLException, IOException {
         URL url = new URL("http://finance.yahoo.com/d/quotes.csv?s=" + symbol
                 + QUOTE_FORMAT);
 
@@ -73,34 +74,42 @@ public class Stock implements Serializable
 
         in.close();
 
-        if (line != null && line.length() > 0)
-        {
+        if (line != null && line.length() > 0) {
             // parse the line and remove quotes where necessary
             String[] values = line.split(",");
             change = values[1].substring(1, values[1].length() - 1);
-            range  = values[2].substring(1, values[2].length() - 1);
-            name   = values[3].substring(1, values[3].length() - 1);
+            range = values[2].substring(1, values[2].length() - 1);
+            name = values[3].substring(1, values[3].length() - 1);
 
             // Since real names can have commas, handle possible rest of name.
-            for (int i = 4;  i < values.length;  ++i)
+            for (int i = 4; i < values.length; ++i)
                 name = name + ", " + values[i].substring(1, values[i].length() - 1);
+
+
 
             if (DEBUG)
                 Log.i(TAG_PREFIX + "Stock.load()", "name = " + name);
 
-            String lastTrade = values[0];
+            if (!name.contains("/")) {
 
-            // parse last trade time
-            int start = 1; // skip opening quote
-            int end = lastTrade.indexOf(" - ");
-            lastTradeTime = lastTrade.substring(start, end);
+                String lastTrade = values[0];
 
-            // parse last trade price
-            start = lastTrade.indexOf(">") + 1;
-            end = lastTrade.indexOf("<", start);
-            lastTradePrice = lastTrade.substring(start, end);
+                // parse last trade time
+                int start = 1; // skip opening quote
+                int end = lastTrade.indexOf(" - ");
+                lastTradeTime = lastTrade.substring(start, end);
+
+                // parse last trade price
+                start = lastTrade.indexOf(">") + 1;
+                end = lastTrade.indexOf("<", start);
+                lastTradePrice = lastTrade.substring(start, end);
+            }
+            else{
+                badName = true;
+            }
         }
     }
+
 
 
     /**
@@ -155,4 +164,6 @@ public class Stock implements Serializable
     {
         return symbol;
     }
+
+    public boolean getOk(){return badName;}
 }
